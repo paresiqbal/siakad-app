caa1ac975fcfd92a77de58a95ec916df77f31626
paresiqbal <thecrowdel@gmail.com>
2024-08-30 23:03:29 +0700

    Refactor username validation in UserController register method

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,4 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-
 Route::post("/users", [UserController::class, "register"]);
