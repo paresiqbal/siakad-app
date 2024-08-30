0a930f3085286825ff3abc0dc391ea52c88f2c03
paresiqbal <thecrowdel@gmail.com>
2024-08-30 23:19:56 +0700

    `Refactor username validation in UserController login method`

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserLoginRequest;
 use App\Http\Requests\UserRegisterRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UserController extends Controller
 {
@@ -25,6 +27,25 @@ class UserController extends Controller
 
         $user = new User($data);
         $user->password = Hash::make($data["password"]);
+        $user->save();
+
+        return new UserResource($user);
+    }
+
+    // Login a user
+    public function login(UserLoginRequest $request): UserResource
+    {
+        $data = $request->validated();
+        $user = User::where("username", $data["username"])->first();
+        if (!$user && Hash::check($data["password"], $user->password) == false) {
+            throw new HttpResponseException(response([
+                "errors" => [
+                    "username" => ["Invalid username or password"],
+                ],
+            ], 400));
+        }
+
+        $user->token = Str::uudi()->toString();
         $user->save();
 
         return new UserResource($user);
