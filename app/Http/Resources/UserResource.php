c6044e75a06c667a71f1b4b993c2837c912d5d33
paresiqbal <thecrowdel@gmail.com>
2024-08-30 22:46:50 +0700

    chore: Remove 'name' field from UserResource toArray method

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -17,7 +17,6 @@ class UserResource extends JsonResource
         return [
             "id" => $this->id,
             "username" => $this->username,
-            "name" => $this->name,
             "token" => $this->whenNotNull($this->token),
         ];
     }
