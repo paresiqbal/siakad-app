aa2a33f1ca8e3bacbd7a53b38bc3a55c9ab7b579
paresiqbal <thecrowdel@gmail.com>
2024-08-31 22:49:43 +0700

    `Add news creation endpoint to API`

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\NewsController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,3 +8,5 @@ use Illuminate\Support\Facades\Route;
 Route::post("/users", [UserController::class, "register"]);
 Route::post("/users/login", [UserController::class, "login"]);
 Route::post('/logout', [UserController::class, 'logout']);
+
+Route::post('/news', [NewsController::class, 'create']);
