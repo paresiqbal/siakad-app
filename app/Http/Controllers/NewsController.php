36d005d997287e88778570ed8028ac3431dc9144
paresiqbal <thecrowdel@gmail.com>
2024-08-31 22:49:00 +0700

    `Refactor NewsController to include news creation functionality`

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\NewsRequest;
+use App\Http\Resources\NewsResource;
+use App\Models\News;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class NewsController extends Controller
 {
-    //
+    public function create(NewsRequest $request): NewsResource
+    {
+        $data = $request->validated();
+
+        // Check if a news article with the same title already exists
+        if (News::where("title", $data["title"])->exists()) {
+            throw new HttpResponseException(response([
+                "errors" => [
+                    "title" => ["News with this title already exists"],
+                ],
+            ], 400));
+        }
+
+        // Create a new news article
+        $news = new News($data);
+        $news->save();
+
+        // Return the newly created news item as a resource
+        return new NewsResource($news);
+    }
 }
