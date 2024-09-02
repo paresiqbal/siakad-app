f7417dbe719820c5ee29f9d3eedbb0ba6c9f26d5
paresiqbal <thecrowdel@gmail.com>
2024-09-02 13:28:32 +0700

    Refactor NewsController to include soft deletes for news table

--- database/migrations/2024_09_02_062743_add_soft_deletes_to_news_table.php
+++ database/migrations/2024_09_02_062743_add_soft_deletes_to_news_table.php
@@ -9,20 +9,17 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::table('news', function (Blueprint $table) {
-            //
+            $table->softDeletes();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::table('news', function (Blueprint $table) {
-            //
+            $table->dropSoftDeletes();
         });
     }
 };
