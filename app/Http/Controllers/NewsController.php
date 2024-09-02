88a16f2238b5a21cdfa52f75f9d0580768bcf5dc
paresiqbal <thecrowdel@gmail.com>
2024-09-02 13:33:07 +0700

    `Refactor NewsController to include lazy loading for images in news index endpoint`

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -6,12 +6,10 @@ use App\Http\Requests\NewsRequest;
 use App\Http\Resources\NewsResource;
 use App\Models\News;
 use Illuminate\Http\Exceptions\HttpResponseException;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
 
 class NewsController extends Controller
 {
-    // Create news
+    // create news
     public function create(NewsRequest $request): NewsResource
     {
         $data = $request->validated();
@@ -25,22 +23,17 @@ class NewsController extends Controller
             ], 400));
         }
 
-        // Check if an image was uploaded
-        if ($request->hasFile('image')) {
-            $path = $request->file('image')->store('news_images', 'public');
-            $data['image'] = $path;
-        }
-
         $news = new News($data);
         $news->save();
 
         return new NewsResource($news);
     }
 
-    // Update news
+    // update news
     public function update(NewsRequest $request, $id): NewsResource
     {
         $data = $request->validated();
+
         $news = News::findOrFail($id);
 
         // Check if a different news article with the same title already exists
@@ -55,23 +48,12 @@ class NewsController extends Controller
             ], 400));
         }
 
-        // Check if a new image was uploaded
-        if ($request->hasFile('image')) {
-            // Delete the old image if it exists
-            if ($news->image) {
-                Storage::disk('public')->delete($news->image);
-            }
-
-            $path = $request->file('image')->store('news_images', 'public');
-            $data['image'] = $path;
-        }
-
         $news->update($data);
 
         return new NewsResource($news);
     }
 
-    // Show specific news
+    // show specific news
     public function show($id): NewsResource
     {
         $news = News::findOrFail($id);
@@ -79,26 +61,19 @@ class NewsController extends Controller
         return new NewsResource($news);
     }
 
-    // Show all news
-    public function index(Request $request)
+    // show all news
+    public function index()
     {
-        $query = News::query();
+        $news = News::paginate(10);
 
-        if ($request->has('title')) {
-            $query->where('title', 'like', '%' . $request->input('title') . '%');
-        }
-
-        if ($request->has('author')) {
-            $query->where('author', $request->input('author'));
-        }
-
-        return NewsResource::collection($query->paginate(10));
+        return NewsResource::collection($news);
     }
 
-    // Delete news
+    // delete news
     public function destroy($id)
     {
         $news = News::findOrFail($id);
+
         $news->delete();
 
         return response()->json(['message' => 'News article deleted successfully.'], 200);
