e94fcb40b4dfeda64f2bc43e425fe0a3c74ed320
paresiqbal <thecrowdel@gmail.com>
2024-09-03 12:53:26 +0700

    `Refactor AgendaController to include create new agenda functionality`

--- app/Http/Controllers/AgendaController.php
+++ app/Http/Controllers/AgendaController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AgendaRequest;
+use App\Http\Resources\AgendaResource;
+use App\Models\Agenda;
+use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\Request;
 
 class AgendaController extends Controller
 {
     //create new agenda
+    public function create(AgendaRequest $request): AgendaResource
+    {
+        $data = $request->validated();
+
+        // Check if a agenda article with the same title already exists
+        if (Agenda::where("title", $data["title"])->exists()) {
+            throw new HttpResponseException(response([
+                "errors" => [
+                    "title" => ["Agenda with this title already exists"],
+                ],
+            ], 400));
+        }
+
+        $agenda = new Agenda($data);
+        $agenda->save();
+
+        return new AgendaResource($agenda);
+    }
 }
