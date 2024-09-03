48ca5dd4e86395ce3223d6aedae297b814387130
paresiqbal <thecrowdel@gmail.com>
2024-09-03 19:32:01 +0700

    `Refactor StaffDirRequest and StaffDirResource to adhere to coding standards`

---
+++ app/Http/Requests/StaffDirRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StaffDirRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'position' => 'required|string|max:255',
+            'department' => 'required|string|max:255',
+            'email' => 'required|email|max:255|unique:staff_directories,email,' . $this->id,
+            'phone' => 'required|string|max:20',
+            'image' => 'nullable|image|max:2048', // Allow null, must be an image, max 2MB
+        ];
+    }
+}
