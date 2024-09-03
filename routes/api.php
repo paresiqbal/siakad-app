b3a7a7793a1a6a03cebfc9f409c4d167daa6c158
paresiqbal <thecrowdel@gmail.com>
2024-09-03 19:56:19 +0700

    `Add staff directory routes and controller methods`

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AgendaController;
 use App\Http\Controllers\NewsController;
+use App\Http\Controllers\StaffDirController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -22,3 +23,9 @@ Route::post('/create-agenda', [AgendaController::class, 'create']);
 Route::put('/update-agenda/{id}', [AgendaController::class, 'update']);
 Route::delete('/delete-agenda/{id}', [AgendaController::class, 'destroy']);
 Route::get('/list-agenda', [AgendaController::class, 'index']);
+
+// staff directory route
+Route::post('/create-staff', [StaffDirController::class, 'create']);
+Route::put('/update-staff/{id}', [StaffDirController::class, 'update']);
+Route::delete('/delete-staff/{id}', [StaffDirController::class, 'destroy']);
+Route::get('/list-staff', [StaffDirController::class, 'index']);
