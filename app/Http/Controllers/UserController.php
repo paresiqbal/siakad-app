3a7900affaa4f99e9733fba967fef31a9ea9c025
paresiqbal <thecrowdel@gmail.com>
2024-09-06 06:59:24 +0700

    Refactor UserController by removing unused import statements

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,6 @@ use App\Models\User;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 class UserController extends Controller
 {
