540f46e67b6bde7d71b401b864eb568d13957329
paresiqbal <thecrowdel@gmail.com>
2024-09-05 21:13:17 +0700

    `Refactor UserController logout method to include authentication middleware`

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 // user route
 Route::post("/users", [UserController::class, "register"]);
 Route::post("/users/login", [UserController::class, "login"]);
-Route::post('/logout', [UserController::class, 'logout'])->middleware('auth:sanctum');
+Route::post('/logout', [UserController::class, 'logout']);
 
 // news route
 Route::post('/create-news', [NewsController::class, 'create']);
