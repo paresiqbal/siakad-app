6f6e321ff747d2147c4cda701734b8bd9114ec04
paresiqbal <thecrowdel@gmail.com>
2024-09-06 20:25:13 +0700

    Refactor routes/api.php to include authentication middleware for user route

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,12 @@ use App\Http\Controllers\AgendaController;
 use App\Http\Controllers\NewsController;
 use App\Http\Controllers\StaffDirController;
 use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 
 // user route
