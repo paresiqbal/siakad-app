4bbf0a1a213ff9ea78accb2b0fd65348ec1f114a
paresiqbal <thecrowdel@gmail.com>
2024-09-06 15:04:08 +0700

    Refactor NewsController create method to use authenticated user's news relationship

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -9,76 +9,9 @@ use Illuminate\Http\Exceptions\HttpResponseException;
 
 class NewsController extends Controller
 {
-    // create news
-    public function create(NewsRequest $request): NewsResource
-    {
-        $data = $request->validated();
-
-        // Check if a news article with the same title already exists
-        if (News::where("title", $data["title"])->exists()) {
-            throw new HttpResponseException(response([
-                "errors" => [
-                    "title" => ["News with this title already exists"],
-                ],
-            ], 400));
-        }
-
-        $news = new News($data);
-        $news->save();
-
-        return new NewsResource($news);
-    }
-
-    // update news
-    public function update(NewsRequest $request, $id): NewsResource
-    {
-        $data = $request->validated();
-
-        $news = News::findOrFail($id);
-
-        // Check if a different news article with the same title already exists
-        if (News::where("title", $data["title"])
-            ->where("id", "<>", $id)
-            ->exists()
-        ) {
-            throw new HttpResponseException(response([
-                "errors" => [
-                    "title" => ["Another news article with this title already exists"],
-                ],
-            ], 400));
-        }
-
-        if ($request->hasFile('image')) {
-            $data['image'] = $request->file('image')->store('images/news', 'public');
-        }
-
-        $news->update($data);
-
-        return new NewsResource($news);
-    }
-
-    // show specific news
-    public function show($id): NewsResource
-    {
-        $news = News::findOrFail($id);
-
-        return new NewsResource($news);
-    }
-
-    // show all news
     public function index()
     {
-        $news = News::paginate(10);
-
+        $news = News::with('user')->paginate(10);
         return NewsResource::collection($news);
-    }
-
-    // delete news
-    public function destroy($id)
-    {
-        $news = News::findOrFail($id);
-        $news->delete();
-
-        return response()->json(['message' => 'News article deleted successfully.'], 200);
     }
 }
