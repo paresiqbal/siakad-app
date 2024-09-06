14cb7de5b03086f902e106964d65dc9002adec31
paresiqbal <thecrowdel@gmail.com>
2024-09-06 19:08:36 +0700

    Final News

--- database/migrations/2024_09_06_120232_drop_author_column_from_news_table.php
+++ database/migrations/2024_09_06_120232_drop_author_column_from_news_table.php
@@ -9,14 +9,14 @@ class DropAuthorColumnFromNewsTable extends Migration
     public function up()
     {
         Schema::table('news', function (Blueprint $table) {
-            $table->dropColumn('author');  // This will drop the 'author' column
+            $table->dropColumn('author');
         });
     }
 
     public function down()
     {
         Schema::table('news', function (Blueprint $table) {
-            $table->string('author')->nullable(); // This will add back the 'author' column if you roll back
+            $table->string('author')->nullable();
         });
     }
 }
